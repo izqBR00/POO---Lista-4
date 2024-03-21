26cc5d2085050941bacd96ac3b0d6367c3dacb2c
Izaque_Nogueira <140656542+izqBR00@users.noreply.github.com>
2024-03-21 08:54:54 -0300

    Add files via upload

---
+++ Lista4/Lista4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Lista4
+{
+    class Lista4
+    {
+        static void Main(string[] args)
+        {
+            Lista4 l = new Lista4();
+            l.choose();
+            
+        }
+
+        Ex1 ex1 = new Ex1();
+        Ex2 ex2 = new Ex2();
+        Ex3 ex3 = new Ex3();
+        Ex4 ex4 = new Ex4();
+        int choice;
+
+        void choose()
+        {
+            Console.WriteLine("Selecione o exercicio\n | 1 | 2 | 3 | 4 |");
+            choice = int.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1: ex1.executeEx1(); break;
+
+                case 2: ex2.executeEx2(); break;
+
+                case 3: ex3.executeEx3(); break;
+
+                case 4: ex4.executeEx4(); break;
+
+                
+            }
+
+        }
+    }
+}
