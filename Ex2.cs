1b140af69a398b66a2300faa189fdb69e9cf58c2
Izaque_Nogueira <140656542+izqBR00@users.noreply.github.com>
2024-03-21 09:23:14 -0300

    Add files via upload

---
+++ Ex2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4
+{
+    internal class Ex2
+    {
+        public void executeEx2()
+        {
+            getData();
+            calc();
+            Console.WriteLine(mensagem);
+        }
+        string s, primeiro, segundo, hora, gen, mensagem;
+        int genero, inthora;
+
+        
+        void getData()
+        {
+            Console.WriteLine("Insira seu nome: ");
+            s = Console.ReadLine();
+            string[] n = s.Split(" ");
+            primeiro = n[0];
+            segundo = n[1];
+
+            Console.WriteLine("Insira seu genero [1] para Masculino e [2] para Feminino");
+            genero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Horario de entrada (hh:mm): ");
+            hora = Console.ReadLine();
+        }
+
+        void calc()
+        {
+            DateTime.TryParseExact(hora, "HH:mm", null, System.Globalization.DateTimeStyles.None, out DateTime horas);
+            inthora = horas.Hour;
+
+            if (inthora < 12 && inthora > 00)
+            {
+                mensagem = "Bom dia";
+            }
+            else if (inthora >= 12 && inthora < 18)
+            {
+                mensagem = "Boa tarde";
+            }
+            else
+            {
+                mensagem = "Boa noite";
+            }
+
+            if (genero == 1)
+            {
+                gen = "Sr.";                
+            }
+            else if (genero == 2) 
+            {
+                gen = "Sra.";         
+            }
+
+            mensagem = mensagem + " " + gen + " " + primeiro + " " + segundo;
+
+
+
+        }
+    }
+}
---
+++ Ex3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4
+{
+    internal class Ex3
+    {
+        public void executeEx3()
+        {
+            getData();
+            calc();
+            Console.WriteLine("Numero do matricula: {0}", numero);
+            Console.WriteLine("Salario do mes: R${0:F2}", salario);
+        }
+
+        int numero;
+        float horas, valorHora, salario;
+        void getData()
+        {
+            Console.WriteLine("Insira o numero de matricula do funcionario: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o numero de horas trabalhadas: ");
+            horas = float.Parse(Console.ReadLine());
+
+            Console.Write("Insira quanto o funcionario recebe por hora: \nR$");
+            valorHora = float.Parse(Console.ReadLine());
+        }
+
+        void calc()
+        {
+            salario = valorHora * horas;
+        }
+
+    }
+}
