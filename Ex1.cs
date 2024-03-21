fa78f1fa86f04cae741de3fe34e04ee4d233821b
Izaque_Nogueira <140656542+izqBR00@users.noreply.github.com>
2024-03-21 08:58:22 -0300

    Add files via upload

---
+++ Ex1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4
+{
+    internal class Ex1
+    {
+        public void executeEx1()
+        {
+            getVal();
+            calcBhaskara();
+            Console.WriteLine("X1 = " + x1 + "\nX2 = " + x2);
+        }
+
+        public double a, b, c, delta, x1, x2;
+        void getVal()
+        {
+            
+            Console.WriteLine("Insira o valor de a: "); 
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor de b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor de c: ");
+            c = int.Parse(Console.ReadLine());
+
+        }
+
+        void calcBhaskara()
+        {
+            delta = (b * b) * (4 * a * c); 
+
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta))/(2*a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            }
+            else
+            {
+                Console.WriteLine("Não ha raizes reais!");
+            }
+            
+        }
+    }
+}
---
+++ Ex4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4
+{
+    internal class Ex4
+    {
+        public void executeEx4()
+        {
+            getData();
+            choice();
+        }
+        double A, B, C;
+        int escolha;
+        void getData()
+        {
+            Console.WriteLine("Seja bem vindo! Insira os valores:");
+            Console.Write("A = ");
+            A = double.Parse(Console.ReadLine());            
+            Console.Write("B = ");
+            B = double.Parse(Console.ReadLine());
+            Console.Write("C = ");
+            C = double.Parse(Console.ReadLine());
+        }
+
+        void choice()
+        {
+            int n = 0;
+            while (n == 0)
+            {
+                Console.WriteLine("Escolha o calculo que deseja realizar:\n[1] a area do triangulo retangulo com base A e altura C\n[2] a area do circulo de raio C\n[3] a area do trapezio que tem base maior A, menor B e altura C\n[4] a area do quadrado de lado B\n[5] a area do retangulo de base A e altura B\n[6] sair");
+                escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1: Console.WriteLine("\nArea do triangulo = " + calcTriangle() + "\n"); break;
+
+                    case 2: Console.WriteLine("\nArea do circulo = " + calcCircle() + "\n"); break;
+
+                    case 3: Console.WriteLine("\nArea do trapezio = " + calcTrapeze() + "\n"); break;
+
+                    case 4: Console.WriteLine("\nArea do quadrado = " + calcSquare() + "\n"); break;
+
+                    case 5: Console.WriteLine("\nArea do retangulo = " + calcRectangle() + "\n"); break;
+
+                    case 6: n = 1; break;
+
+                    default: Console.WriteLine("Insira uma opção válida"); break;
+                }
+            }
+            
+            
+        }
+
+        public double calcTriangle()
+        {
+            return (A * C) / 2;
+        }
+
+        public double calcCircle()
+        {
+            return 3.14 * C * C;
+        }
+
+        public double calcTrapeze()
+        {
+            return ((A + B) * C) / 2; 
+        }
+
+        public double calcSquare()
+        {
+            return B * B;
+        }
+
+        public double calcRectangle()
+        {
+            return A * B;
+        }
+    }
+}
